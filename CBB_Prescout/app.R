32fe6629f7f3b166d09054aea36b546299c1ec54
Jeremy Patak <jeremypatak16@gmail.com>
2023-01-23 18:44:13 -0600

    add SR team stats df into app

--- CBB_Prescout/app.R
+++ CBB_Prescout/app.R
@@ -8,10 +8,78 @@ opponentList = c("Washington State", "UCLA")
 opponentSRurl_db = read_xlsx("opp_url.xlsx")
 year=2023
 
+#####basic offense table webscrape
+basic_offense_url = "https://www.sports-reference.com/cbb/seasons/2023-school-stats.html"
+
+basic_offense = read_html(basic_offense_url) %>%
+  html_table(fill = TRUE) %>%
+  .[[1]] %>% as.data.frame() %>%
+  .[,-c(9,12,15,18,21)] %>% #remove empty columns
+  row_to_names(row_number = 1, remove_row = T) %>% clean_names() %>% #use row 1 as col names
+  filter(!g=='G') %>% filter(!g=="Overall") %>% #filter out 'header' rows scattered in the data frame
+  mutate(school = ifelse(endsWith(school, "NCAA"), substr(school, 1, nchar(school)-5), school)) %>% #removes tag given on SR for making the NCAAT
+  .[,-c(1, 9:16)] # removes columns for conference, home, and away team record
+#add offense label to col names
+colnames(basic_offense)= c(colnames(basic_offense[,1:7]), paste("offense_", colnames(basic_offense[,8:24]), sep = ""))
 
 
+#####basic defense table webscrape
+basic_defense_url = "https://www.sports-reference.com/cbb/seasons/2023-opponent-stats.html"
+
+basic_defense = read_html(basic_defense_url) %>%
+  html_table(fill = TRUE) %>%
+  .[[1]] %>% as.data.frame() %>%
+  .[,-c(9,12,15,18,21)] %>% #remove empty columns
+  row_to_names(row_number = 1, remove_row = T) %>% clean_names() %>% #use row 1 as col names
+  filter(!g=='G') %>% filter(!g=="Overall") %>% #filter out 'header' rows scattered in the data frame
+  mutate(school = ifelse(endsWith(school, "NCAA"), substr(school, 1, nchar(school)-5), school)) %>% #removes tag given on SR for making the NCAAT
+  .[,-c(1, 9:16)] #removes columns for conference, home, and away team records
+#add defense label to col names
+colnames(basic_defense)= c(colnames(basic_defense[,1:7]), paste("defense_", colnames(basic_defense[,8:24]), sep = ""))
 
 
+#####advanced offense table webscrape
+advanced_offense_url = "https://www.sports-reference.com/cbb/seasons/2023-advanced-school-stats.html"
+
+advanced_offense = read_html(advanced_offense_url) %>%
+  html_table(fill = TRUE) %>%
+  .[[1]] %>% as.data.frame() %>%
+  .[,-c(9,12,15,18,21)] %>% #remove empty columns
+  row_to_names(row_number = 1, remove_row = T) %>% clean_names() %>% #use row 1 as col names
+  filter(!g=='G') %>% filter(!g=="Overall") %>% #filter out 'header' rows scattered in the data frame
+  mutate(school = ifelse(endsWith(school, "NCAA"), substr(school, 1, nchar(school)-5), school)) %>% #removes tag given on SR for making the NCAAT
+  .[,-c(1, 9:16)] #removes columns for conference, home, and away team records
+colnames(advanced_offense)= c(colnames(advanced_offense[,1:7]), paste("offense_", colnames(advanced_offense[,8:20]), sep = ""))
+
+#####advanced defense table webscrape
+advanced_defense_url = "https://www.sports-reference.com/cbb/seasons/2023-advanced-opponent-stats.html"
+
+advanced_defense = read_html(advanced_defense_url) %>%
+  html_table(fill = TRUE) %>%
+  .[[1]] %>% as.data.frame() %>%
+  .[,-c(9,12,15,18,21)] %>% #remove empty columns
+  row_to_names(row_number = 1, remove_row = T) %>% clean_names() %>% #use row 1 as col names
+  filter(!g=='G') %>% filter(!g=="Overall") %>% #filter out 'header' rows scattered in the data frame
+  mutate(school = ifelse(endsWith(school, "NCAA"), substr(school, 1, nchar(school)-5), school)) %>%
+  .[,-c(1, 9:16)] #removes columns for conference, home, and away team records
+colnames(advanced_defense)= c(colnames(advanced_defense[,1:7]), paste("defense_", colnames(advanced_defense[,8:20]), sep = ""))
+
+#put all tables into one 
+SR_team_stats = left_join(basic_offense, basic_defense, by=c("school", "g", "w", "l", "w_l_percent", "srs", "sos"))
+SR_team_stats = left_join(SR_team_stats, advanced_offense, by=c("school", "g", "w", "l", "w_l_percent", "srs", "sos"))
+SR_team_stats = left_join(SR_team_stats, advanced_defense, by=c("school", "g", "w", "l", "w_l_percent", "srs", "sos"))
+
+rm(advanced_offense, advanced_defense, basic_offense, basic_defense, advanced_offense_url, advanced_defense_url, basic_offense_url, basic_defense_url)
+
+#convert stats into usable numeric stats
+for(c in 2:67){SR_team_stats[,c] = SR_team_stats[,c] %>% as.numeric()}; rm(c)
+
+#make all stats we want using other stats
+SR_team_stats = SR_team_stats %>% mutate(offense_x2p_percent = (offense_fg - offense_x3p)/(offense_fga - offense_x3pa),
+                                         offense_drb_percent = 100 - defense_orb_percent)
+
+#standardize school names
+SR_team_stats = SR_team_stats %>% mutate(school = clean_school_names(school))
 
 
 
