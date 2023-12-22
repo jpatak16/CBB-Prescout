58bc9bf86f47afeb95fd966f04bf68e06cf3b37e
jpatak16 <jeremypatak16@gmail.com>
2023-12-22 19:25:20 +0000

    file to download data for my apps to be used by cron

---
+++ data_download.R
@@ -0,0 +1,4 @@
+pb_download(repo = "jpatak16/CBB-Prescout", dest = "Clemson/data", overwrite = T)
+pb_download(repo = "jpatak16/CBB-Prescout", dest = "Oregon/data", overwrite = T)
+pb_download(repo = "jpatak16/CBB-Prescout", dest = "NewMexico/data", overwrite = T)
+pb_download(repo = "jpatak16/CBB-Prescout", dest = "MississippiState/data", overwrite = T)
